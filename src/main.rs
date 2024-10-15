9eb14aa94710c6cecdb44931b7deb044336fe5c2
James Starkman <james@starkman.net>
2024-10-14 23:04:25 -0400

    First draft of logic and presentation
    Known issues:
    + Cannot lose the game
    + Hyperactive inputs
    + Pieces sometimes go invisible (middle-left of screen)
    + Pieces drop into cleared rows, but do not keep dropping

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,73 @@
+mod tetris;
+
 use macroquad::prelude::*;
+use macroquad::color;
+use miniquad::window::set_window_size;
 
-#[macroquad::main("BasicShapes")]
+#[macroquad::main("Tetris clone in Rust")]
 async fn main() {
-    loop {
-        clear_background(RED);
+	// HARDCODE Do a proper config system later
+	let width_cells = 8;
+	let height_cells = 24;
+	let cell_sidelength_px = 32;
+	// derived config
+	let width_px = width_cells * cell_sidelength_px;
+	let height_px = height_cells * cell_sidelength_px;
+	let score_font_size = (cell_sidelength_px as u16) * 2;
+	let cell_sidelength_px_f32 = cell_sidelength_px as f32;
+	// </config>
+	let mut game_state = tetris::GameState::new(height_cells, width_cells);
+	loop {
+		// Input
+		// FIXME framerate?
+		if is_key_down(KeyCode::Space) {
+			game_state.toggle_drop_rate();
+		}
+		// Only one direction at once, please.
+		if is_key_down(KeyCode::Up) {
+			game_state.try_rotate_current_piece(false);
+		} else if is_key_down(KeyCode::Down) {
+			game_state.try_rotate_current_piece(true);
+		} else if is_key_down(KeyCode::Left) {
+			game_state.try_leftright_current_piece(true);
+		} else if is_key_down(KeyCode::Right) {
+			game_state.try_leftright_current_piece(false);
+		}
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+		// Logic
+		game_state.tick();
 
-        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
+		// Draw
+		clear_background(BLACK);
+		set_window_size(width_px as u32, height_px as u32);
 
-        next_frame().await
-    }
+		let score = game_state.rows_cleared.to_string();
+		let score_dims = measure_text(&score, None, score_font_size, 1.0);
+		draw_text(&score, (width_px as f32 - score_dims.width) / 2.0, (height_px as f32 - score_dims.height) / 2.0, score_font_size as f32, DARKGRAY);
+
+		let (mut x, mut y) = (0.0, 0.0);
+		for row in game_state.cell_matrix.iter() {
+			for cell in row.cells.iter() {
+				if let Some(c) = cell {
+					let color = color::hsl_to_rgb(c.hue, 1.0, 0.5); // HARDCODE Maybe less saturated?
+					draw_rectangle(x, y, cell_sidelength_px_f32, cell_sidelength_px_f32, color);
+				}
+				x += cell_sidelength_px_f32;
+			}
+			x = 0.0;
+			y += cell_sidelength_px_f32;
+		}
+
+		if let Some(p) = game_state.current_piece.as_ref() {
+			for c in p.cells.iter() {
+				let color = color::hsl_to_rgb(c.cell.hue, 0.5, 0.5); // HARDCODE Maybe even less saturated?
+				let (x, y) = (c.x as f32, c.y as f32);
+				let (gx, gy) = (game_state.current_piece_mass_xy.0 as f32, game_state.current_piece_mass_xy.1 as f32);
+				let (x_px, y_px) = ((gx + x) * cell_sidelength_px_f32, (gy + y) * cell_sidelength_px_f32);
+				draw_rectangle(x_px, y_px, cell_sidelength_px_f32, cell_sidelength_px_f32, color);
+			}
+		}
+
+		next_frame().await
+	}
 }
---
+++ src/tetris.rs
@@ -0,0 +1,235 @@
+pub struct GameState {
+	/// Indexing: cell_matrix[y].cells[x] = Some(foo_cell);
+	pub cell_matrix: Vec<Row>,
+	pub cell_matrix_width: usize,
+	/// None during row clears
+	pub current_piece: Option<Piece>,
+	/// Global coordinates of the center of mass of this piece; may or may not have a Cell.
+	pub current_piece_mass_xy: (usize, usize),
+	/// Time to fall by one cell-space, expressed in game ticks.
+	current_piece_ticks_per_drop_want: u32,
+	/// Time already spent falling by one cell-space, expressed in game ticks.
+	current_piece_ticks_per_drop_have: u32,
+	/// Counter; never decremented.
+	pub rows_cleared: u32,
+}
+
+impl GameState {
+	pub fn new(height: usize, width: usize) -> GameState {
+		let mut gs = Self {
+			cell_matrix: (0 .. height).map(|_| Row::new(width)).collect(),
+			cell_matrix_width: width,
+			current_piece: None, // generated below
+			current_piece_mass_xy: (0, 0), // ibid
+			current_piece_ticks_per_drop_want: 10, // HARDCODE see other writes to this field
+			current_piece_ticks_per_drop_have: 0,
+			rows_cleared: 0,
+		};
+		gs.queue_new_piece();
+		gs
+	}
+	pub fn try_rotate_current_piece(&mut self, clockwise: bool) -> bool {
+		if let Some(p_old) = self.current_piece.as_ref() {
+			let p_new = p_old.rotated(clockwise);
+			let dst = (self.current_piece_mass_xy.0 as isize, self.current_piece_mass_xy.1 as isize);
+			if self.can_place(&p_new, dst) {
+				self.current_piece = Some(p_new);
+				return true;
+			}
+		}
+		false
+	}
+
+	pub fn try_leftright_current_piece(&mut self, leftwards: bool) -> bool {
+		if let Some(p) = self.current_piece.as_ref() {
+			let direction = if leftwards { -1 } else { 1 };
+			let dst = (self.current_piece_mass_xy.0 as isize + direction, self.current_piece_mass_xy.1 as isize);
+			if self.can_place(&p, dst) {
+				self.current_piece_mass_xy = (dst.0 as usize, dst.1 as usize);
+				return true;
+			}
+		}
+		false
+	}
+
+	pub fn tick(&mut self) -> () {
+		self.current_piece_ticks_per_drop_have += 1;
+		if self.current_piece_ticks_per_drop_have < self.current_piece_ticks_per_drop_want {
+			return;
+		}
+		self.current_piece_ticks_per_drop_have = 0;
+		if let Some(p) = self.current_piece.as_ref() {
+			let dst = (self.current_piece_mass_xy.0 as isize, self.current_piece_mass_xy.1 as isize + 1);
+			if self.can_place(&p, dst) {
+				self.current_piece_mass_xy = (dst.0 as usize, dst.1 as usize);
+			} else {
+				self.commit_current_piece();
+				self.clear_finished_rows();
+			}
+		} else {
+			self.queue_new_piece();
+		}
+	}
+
+	fn commit_current_piece(&mut self) -> () {
+		if let Some(p) = self.current_piece.take() {
+			for c in p.cells.into_iter() {
+				let x = (self.current_piece_mass_xy.0 as i32 + c.x) as usize;
+				let y = (self.current_piece_mass_xy.1 as i32 + c.y) as usize;
+				self.cell_matrix[y].cells[x] = Some(c.cell);
+				self.cell_matrix[y].is_empty = false;
+			}
+		}
+	}
+
+	fn clear_finished_rows(&mut self) -> () {
+		// clear and drop rows; is bubble-sort in slow motion
+		let mut anything_changed = false;
+		for i_row in 0 .. self.cell_matrix.len() {
+			{
+				let row = &mut self.cell_matrix[i_row];
+				if row.is_empty {
+					continue;
+				}
+				if row.cells.iter().all(Option::is_some) {
+					row.cells.iter_mut().for_each(|c| { c.take(); });
+					self.rows_cleared += 1;
+					row.is_empty = true;
+					anything_changed = true;
+				}
+			}
+			// drop higher cells
+			if self.cell_matrix[i_row].is_empty {
+				for i_row in (1 ..= i_row).rev() {
+					let i_above = i_row - 1;
+					if !self.cell_matrix[i_above].is_empty {
+						self.cell_matrix.swap(i_row, i_row-1);
+					} else {
+						break;
+					}
+				}
+			}
+		}
+		// FIXME handle multi-clears; maybe take everything above the cleared line and bundle it into a super-Piece?
+		// loop until no more clears
+		if anything_changed {
+			self.clear_finished_rows();
+		}
+	}
+
+	fn queue_new_piece(&mut self) -> () {
+		let p = Piece::generate_new();
+		self.current_piece = Some(p);
+		self.current_piece_mass_xy = (self.cell_matrix_width / 2, 0); // HARDCODE Should this be random?
+	}
+
+	fn can_place(&self, p: &Piece, (global_x, global_y): (isize, isize)) -> bool {
+		let (height, width) = (self.cell_matrix.len(), self.cell_matrix_width);
+		for c in p.cells.iter() {
+			let global_cell_x = global_x + (c.x as isize);
+			if global_cell_x < 0 || width <= (global_cell_x as usize) {
+				return false;
+			}
+			let global_cell_y = global_y + (c.y as isize);
+			if global_cell_y < 0 || height <= (global_cell_y as usize) {
+				return false;
+			}
+			if self.cell_matrix[global_cell_y as usize].cells[global_cell_x as usize].is_some() {
+				return false;
+			}
+		}
+		true
+	}
+
+	pub fn toggle_drop_rate(&mut self) -> () {
+		// HARDCODE Faster/slower falling speeds
+		if self.current_piece_ticks_per_drop_want == 1 {
+			self.current_piece_ticks_per_drop_want = 10;
+		} else {
+			self.current_piece_ticks_per_drop_want = 1;
+		}
+	}
+}
+
+pub struct Row {
+	pub cells: Vec<Option<Cell>>,
+	is_empty: bool,
+}
+
+impl Row {
+	fn new(width: usize) -> Row {
+		Self {
+			cells: vec![None; width],
+			is_empty: true,
+		}
+	}
+}
+
+#[derive(Clone)]
+pub struct Piece {
+	/// May replace with Vec<_> for penta/hex-tetris.
+	pub cells: Vec<CellWithRelativePosition>,
+	// Origin for cell positions
+	pub center_of_mass_x: i32,
+	pub center_of_mass_y: i32,
+}
+
+impl Piece {
+	fn generate_new() -> Piece {
+		let hue: f32 = rand::random();
+		Self {
+			// FIXME placeholder
+			cells: vec![
+				CellWithRelativePosition { cell: Cell::new(hue), x: 0, y: 0, },
+				CellWithRelativePosition { cell: Cell::new(hue), x: 1, y: 0, },
+				CellWithRelativePosition { cell: Cell::new(hue), x: 2, y: 0, },
+				CellWithRelativePosition { cell: Cell::new(hue), x: 3, y: 0, }
+			],
+			center_of_mass_x: 1,
+			center_of_mass_y: 1,
+		}
+	}
+
+	fn rotated(&self, clockwise: bool) -> Piece {
+		let cells = self.cells.iter()
+			.map(|p| {
+				let v = (p.x - self.center_of_mass_x, p.y - self.center_of_mass_y);
+				let v = rotate_2d(clockwise, v);
+				let v = (v.0 + self.center_of_mass_x, v.1 + self.center_of_mass_y);
+				CellWithRelativePosition {
+					cell: p.cell.clone(),
+					x: v.0,
+					y: v.1,
+				}
+			})
+			.collect();
+		Self { cells, ..*self }
+	}
+}
+
+fn rotate_2d(clockwise: bool, (x, y): (i32, i32)) -> (i32, i32) {
+	if clockwise {
+		(y, -x)
+	} else {
+		(-y, x)
+	}
+}
+
+#[derive(Clone)]
+pub struct CellWithRelativePosition {
+	pub cell: Cell,
+	pub x: i32,
+	pub y: i32,
+}
+
+
+#[derive(Clone)]
+pub struct Cell {
+	pub hue: f32,
+}
+
+impl Cell {
+	pub fn new(hue: f32) -> Cell {
+		Self { hue }
+	}
+}
